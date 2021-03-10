e5515630a307f7f8fcbf57d0248796dc4af84560
Patrick Dunn <pmdunn78@gmail.com>
2021-03-10 12:15:11 -0600

    monitoring checked status for each switch

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 $(document).ready(() => {
     console.log('script ready');
 
+    var good = $('#good');
+    var fast = $('#fast');
+    var cheap = $('#cheap');
+
+    const runCheck = el => {
+        if (el[0].checked) {
+            console.log(el[0].id + ' ' + el[0].checked)
+        } else {
+            console.log(el[0].id + ' ' + el[0].checked)
+        }
+    }
+
+    good.on('change', function() {
+        runCheck(good);
+    })
+
+    fast.on('change', function() {
+        runCheck(fast);
+    })
+
+    cheap.on('change', function() {
+        runCheck(cheap);
+    })
+
+    
+
 });
 
